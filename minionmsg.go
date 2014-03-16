f0d17c4283b4252b6d5cac6b8e0275f538a6765e
Bertrand Janin <b@janin.com>
2014-03-16 19:42:12 -0400

    Make the minion name mandatory and passed before MINIONMSG.

--- minionmsg.go
+++ minionmsg.go
@@ -1,5 +1,11 @@
 // Copyright 2014, Truveris Inc. All Rights Reserved.
 // Use of this source code is governed by the ISC license in the LICENSE file.
+//
+// Defines all the tools to handle the MINIONMSG messages coming from the
+// minions.
+//
+// TODO: We need to reject unauthenticated messages (wrong user id).
+//
 
 package ygor
 
@@ -10,10 +16,13 @@ import (
 
 var (
 	// Detect a MINIOMSG (minion communications).
-	reMinionMsg = regexp.MustCompile(`^MINIONMSG (.*)`)
+	reMinionMsg = regexp.MustCompile(`^([^\s]+) MINIONMSG (.*)`)
 )
 
 type MinionMsg struct {
+	// Name of the minion sending this message.
+	Name string
+
 	// The body of the message as received from the minion.
 	Body string
 
@@ -29,12 +38,13 @@ func NewMinionMsg(line string) *MinionMsg {
 	}
 
 	msg := &MinionMsg{
-		Body: tokens[1],
+		Name: tokens[1],
+		Body: tokens[2],
 	}
 
 	tokens = strings.Split(msg.Body, " ")
 	msg.Command = tokens[0]
-	if len(tokens) > 2 {
+	if len(tokens) > 1 {
 		msg.Args = append(msg.Args, tokens[1:]...)
 	}
 
