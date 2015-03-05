694d88ae89c2f68efd9630ae0e7f97d8a62d2641
Bertrand Janin <b@janin.com>
2015-03-05 09:48:23 -0500

    Remove africa.

--- ygord/module_play.go
+++ ygord/module_play.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-// PlayModule controls the 'play' and 'africa' commands.
+// PlayModule controls the 'play'
 type PlayModule struct{}
 
 // PrivMsg is the message handler for 'play' requests.
@@ -34,38 +34,12 @@ func (module *PlayModule) PrivMsg(msg *Message) {
 	SendToChannelMinions(msg.ReplyTo, cmd)
 }
 
-// PrivMsgAfrica is the message handler for 'africa' requests.
-func (module *PlayModule) PrivMsgAfrica(msg *Message) {
-	msg.Args = append([]string{"tunes/africa.ogg"}, msg.Args...)
-
-	module.PrivMsg(msg)
-
-	go func() {
-		time.Sleep(2 * time.Second)
-		IRCPrivAction(msg.ReplyTo, "hears the drums echoing tonight,")
-		time.Sleep(5 * time.Second)
-		IRCPrivMsg(msg.ReplyTo, "But she hears only whispers of some quiet conversation")
-		time.Sleep(9 * time.Second)
-		IRCPrivMsg(msg.ReplyTo, "She's coming in the 12:30 flight")
-		time.Sleep(3 * time.Second)
-		IRCPrivMsg(msg.ReplyTo, "The moonlit wings reflect the stars that guide me towards salvation")
-	}()
-}
-
 // Init registers all the commands for this module.
 func (module *PlayModule) Init() {
 	RegisterCommand(Command{
 		Name:            "play",
 		PrivMsgFunction: module.PrivMsg,
 		Addressed:       true,
-		AllowPrivate:    false,
-		AllowChannel:    true,
-	})
-
-	RegisterCommand(Command{
-		Name:            "africa",
-		PrivMsgFunction: module.PrivMsgAfrica,
-		Addressed:       false,
 		AllowPrivate:    false,
 		AllowChannel:    true,
 	})
