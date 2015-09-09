c6b54da27ce009edd936ce7bb6b52b1dcf7c73f7
Chris NeJame <cnejame@truveris.com>
2015-09-09 11:55:17 -0400

    cleaned up comments

--- ygord/media_utils.go
+++ ygord/media_utils.go
@@ -9,34 +9,52 @@ import (
 	"strings"
 )
 
-// validates command usage, and returns a map array containing the arguments
-// for each media item in the list
+// ParseArgList validates command usage, and returns a map array containing the
+// arguments for each media item in the list.
 func parseArgList(argArr []string) ([]map[string]string, error) {
-	// make it a single string
+	// Combine the arguments into a single string
 	mediaListStr := strings.Join(argArr, " ")
-	// media list should be deliminated by commas
+	// Now split at the commas to separate each media item so they can each be
+	// validated individually.
 	mediaListArr := regexp.MustCompile(`,\s*`).Split(mediaListStr, -1)
+	// Make the map array that will house the formatted media items.
 	mediaList := []map[string]string{}
 	for _, media := range mediaListArr {
+		// Split the individual media item at the spaces to seperate the
+		// arguments.
 		mediaArgs := strings.Split(media, " ")
+		// Validate that it has an appropriate number of arguments.
 		if len(mediaArgs) < 1 || len(mediaArgs) > 3 {
 			err := errors.New("invalid number of arguments")
 			return mediaList, err
 		}
+		// Start making the map that represents this media item.
 		m := make(map[string]string)
+		// Insert the URL of this media item into the map that represents it.
 		m["url"] = mediaArgs[0]
+		// Get the start and end bounds passed for this media item (if they
+		// were passed).
 		start, end, err := getBounds(mediaArgs)
 		if err != nil {
 			return mediaList, err
 		}
+		// Insert the start and end bounds into the map that represents this
+		// media item.
 		m["start"] = start
 		m["end"] = end
+		// Append the now completely formed map that represents this media item
+		// into the map array that repesents all the media items passed by this
+		// command.
 		mediaList = append(mediaList, m)
 	}
+	// Return the completed map array that represents all the media items
+	// passed by this command.
 	return mediaList, nil
 }
 
-// grabs starting and ending time frame bounds if either is passed
+// getBounds grabs starting and ending time frame bounds if either is passed.
+// If a bound isn't passed, an empty string is returned to represent that
+// bound.
 func getBounds(args []string) (string, string, error) {
 	sBound := ""
 	eBound := ""
@@ -73,6 +91,5 @@ func getBounds(args []string) (string, string, error) {
 		}
 	}
 
-	//everything's good
 	return sBound, eBound, nil
 }
