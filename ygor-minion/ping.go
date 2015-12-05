0955c3601f6bfcdd54216103f6ab9e533ced1e58
Bertrand Janin <b@janin.com>
2015-12-04 19:00:50 -0500

    Drop the native ygor-minion.

--- ygor-minion/ping.go
+++
@@ -1,9 +0,0 @@
-// Copyright 2014-2015, Truveris Inc. All Rights Reserved.
-
-package main
-
-// Ping replies to ygord with the same timestamp, helping it estimate the
-// duration of a round-trip to this minion.
-func Ping(timestamp string) {
-	Send("pong " + timestamp)
-}
